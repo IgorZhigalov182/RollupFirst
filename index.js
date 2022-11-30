89a6abbb0e22d0612e32b01fcd4cd3dc8c26a389
Igor <zhigalovzenit112233@gmail.com>
2022-11-30 11:16:04 +0300

    second

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import "index.css";
+// const imageJS = require('imageJS');
+import imageJS from './assets/pic.jpg';
+
 console.log('Hello World :)');
+
+const jsImage = document.createElement('img');
+jsImage.className = 'jsImg';
+jsImage.src = imageJS;
+
+document.body.append(jsImage);
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
+import { babel } from '@rollup/plugin-babel';
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+
 export default {
-    input: 'index.js',
+    input: './index.js',
     output: {
       file: './build/bundle.js',
       format: 'cjs'
     },
+    plugins: [
+      babel({ babelHelpers: 'bundled' }),
+      styles(),
+      image({
+        limit: 100000
+      }),
+    ],
     watch: {
         exclude: 'node_modules/**'
     }
